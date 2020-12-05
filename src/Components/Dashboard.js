11ed5648bec9ef17688e39fed9df6c979d0099ec
Shubham Chatterjee <c.suvam@gmail.com>
2020-12-06 00:08:14 +0530

    logic updates

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,16 +4,15 @@ import { Avatar, Grid, Typography } from '@material-ui/core';
 import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-
-
 //seperate map component as a custom hook
 function Map() {
     return (
-        <GoogleMap defaultZoom={7}
+        <GoogleMap defaultZoom={8}
             defaultCenter={{ lat: Number(localStorage.getItem('lat')), lng: Number(localStorage.getItem('lng')) }} />
     )
 }
-
+//rendering the map component as a seperate custom hook component
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 function Dashboard(props) {
 
     const [data, setdata] = useState([]);
@@ -21,9 +20,9 @@ function Dashboard(props) {
     const [LAT, setlat] = useState(null);
     const [LNG, setlng] = useState(null);
     const [showcities, setshowcities] = useState(true);
-    let placeName = "Gujarat";
-    //rendering the map component as a seperate custom hook component
-    const WrappedMap = withScriptjs(withGoogleMap(Map));
+    const [placeName, setplaceName] = useState('india');
+    const [force, setforce] = useState(null);
+
     useEffect(() => {
         let mydata = fetch('/artivatic.json',
             {
@@ -34,47 +33,38 @@ function Dashboard(props) {
             })
         mydata.then(res => res.json()).then(json => setdata(json.states));
 
-
         //using place Name to find out the lat and longitudes
         const googleMapScript = document.createElement("script");
         googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&libraries=places`;
         googleMapScript.async = true;
         window.document.body.appendChild(googleMapScript);
         googleMapScript.addEventListener("load", () => {
-            getLatLng();
+            getlang();
         });
     }, []);
 
-    const getLatLng = () => {
-        var lat, lng, placeId;
-        //using Geocoder API to calculate the lat and longitudes from any place name
-        new window.google.maps.Geocoder().geocode(
-            { address: placeName },
-            function (results, status) {
-                if (status === window.google.maps.GeocoderStatus.OK) {
-                    placeId = results[0].place_id;
-                    lat = results[0].geometry.location.lat();
-                    lng = results[0].geometry.location.lng();
-                    setlat(Number(lat));
-                    setlng(Number(lng));
-                }
-            }
-        );
-    };
-
     useEffect(() => {
-        console.log(data)
-        console.log(LAT);
-        console.log(LNG);
-        if (LAT)
+        if (LAT && placeName)
             localStorage.setItem('lat', LAT);
-        if (LNG)
+        if (LNG && placeName)
             localStorage.setItem('lng', LNG);
 
-        //storing the lat and longs in local storage
-    }, [data, LAT, LNG])
+        //using place Name to find out the lat and longitudes
+        if ((force == 'filled_with_new_district' && placeName) || (force == 'filled_with_new_city' && placeName)) {
+            const googleMapScript = document.createElement("script");
+            googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&libraries=places`;
+            googleMapScript.async = true;
+            window.document.body.appendChild(googleMapScript);
+            googleMapScript.addEventListener("load", () => {
+                getlang();
+            });
+        }
+
+    }, [LAT, LNG, placeName, force])
 
     const pickCityGetDistrict = (str) => {
+        setforce('filled_with_new_city');
+        setplaceName(str);
         setshowcities(false);
         var res = str, district = [];
         if (data) {
@@ -87,6 +77,24 @@ function Dashboard(props) {
         if (district)
             setdismaps(district);
     }
+    const getlang = () => {
+        let lat, lng, placeId;
+        //using Geocoder API to calculate the lat and longitudes from any place name
+        new window.google.maps.Geocoder().geocode(
+            { address: `${placeName}` },
+            function (results, status) {
+                if (status === window.google.maps.GeocoderStatus.OK) {
+                    placeId = results[0].place_id;
+                    lat = results[0].geometry.location.lat();
+                    lng = results[0].geometry.location.lng();
+                    setlat(Number(lat));
+                    setlng(Number(lng));
+                    setforce('got_new_data_for_new_map');
+                }
+            }
+        );
+    }
+
     const logout_option = () => {
         window.location.assign('/logout');
     }
@@ -121,7 +129,6 @@ function Dashboard(props) {
                 <Avatar onClick={logout_option} style={{ cursor: 'pointer' }} src={`${localStorage.getItem('img_url')}`} />
             </div>
 
-
             <div className='bgimage'>
 
                 {data && showcities &&
@@ -138,23 +145,28 @@ function Dashboard(props) {
                     <Grid container className='district_grid' style={{ textAlign: 'center' }}>
                         {dismaps.map(item =>
                             <Grid key={item} item sm={6} md={6} xs={6}>
-                                <Typography className='typo_districts'>{item}</Typography>
+                                <Typography onClick={() => {
+                                    setplaceName(item);
+                                    setforce('filled_with_new_district');
+                                }} className='typo_districts'>{item}</Typography>
                                 <br /><br />
                             </Grid>)}
                     </Grid>
                 }
             </div>
 
+            {placeName && !showcities && force == 'got_new_data_for_new_map' &&
+                < Grid className='map_Grid' style={{ width: "95%", height: "95%" }}>
+                    <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY
+                        }`}
+                        loadingElement={<div style={{ height: `100%`, width: '100%' }} />}
+                        containerElement={<div style={{ height: `100%`, width: `100%` }} />}
+                        mapElement={<div style={{ height: `100%`, width: `100%` }} />} />
+                </Grid>
+            }
 
 
 
-            {/* <div style={{ width: "500px", height: "500px" }}>
-                <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY
-                    }`}
-                    loadingElement={<div style={{ height: `500px`, width: '500px' }} />}
-                    containerElement={<div style={{ height: `500px`, width: `500px` }} />}
-                    mapElement={<div style={{ height: `500px`, width: `500px` }} />} />
-            </div>*/}
 
         </div >
     );
